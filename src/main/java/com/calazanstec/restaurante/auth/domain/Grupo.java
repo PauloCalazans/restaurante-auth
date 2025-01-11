dfc2473d46bfbbe5067ef783d7b9b4a322c2c47a
paulocalazans <paulo.pc355@gmail.com>
2025-01-11 19:01:22 -0300

    Adicionando Authorities ao token, baseado nos grupos do usuário

--- src/main/java/com/calazanstec/restaurante/auth/domain/Grupo.java
+++ src/main/java/com/calazanstec/restaurante/auth/domain/Grupo.java
@@ -7,33 +7,22 @@ import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
-
-/**
- * @author Paulo Calazans on 11/01/2025
- */
-
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-public class Usuario {
+public class Grupo {
 
 	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
+	
 	@Column(nullable = false)
 	private String nome;
 	
-	@Column(nullable = false)
-	private String email;
-	
-	@Column(nullable = false)
-	private String senha;
-
 	@ManyToMany
-	@JoinTable(name = "usuario_grupo", joinColumns = @JoinColumn(name = "usuario_id"),
-			inverseJoinColumns = @JoinColumn(name = "grupo_id"))
-	private Set<Grupo> grupos = new HashSet<>();
-
+	@JoinTable(name = "grupo_permissao", joinColumns = @JoinColumn(name = "grupo_id"),
+			inverseJoinColumns = @JoinColumn(name = "permissao_id"))
+	private Set<Permissao> permissoes = new HashSet<>();
+	
 }
--- src/main/java/com/calazanstec/restaurante/auth/domain/JpaUserDetailService.java
+++ src/main/java/com/calazanstec/restaurante/auth/domain/JpaUserDetailService.java
@@ -2,10 +2,16 @@ package com.calazanstec.restaurante.auth.domain;
 
 import com.calazanstec.restaurante.auth.core.AuthUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 /**
  * @author Paulo Calazans on 11/01/2025
@@ -17,11 +23,19 @@ public class JpaUserDetailService implements UserDetailsService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Transactional(readOnly = true)
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         var usuario = usuarioRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("Usuário  não encontrado com o e-mail informado"));
 
-        return new AuthUser(usuario);
+        return new AuthUser(usuario, getAuthorities(usuario));
+    }
+
+    private Collection<GrantedAuthority> getAuthorities(Usuario usuario) {
+        return usuario.getGrupos().stream()
+                .flatMap(grupo -> grupo.getPermissoes().stream())
+                .map(permissao -> new SimpleGrantedAuthority(permissao.getNome().toUpperCase()))
+                .collect(Collectors.toSet());
     }
 }
